376294f7e61f0905c292ee63c07199a7c04727ad
Jan M. Knaup <janknaup@gmail.com>
2011-02-14 17:20:33 +0100

    *FIXED polynomFunction usig (r)**n instead of (rcut-r)**n

--- barrier/src/comatsci/Calculators/Potentials/polynomFunction.py
+++ barrier/src/comatsci/Calculators/Potentials/polynomFunction.py
@@ -85,7 +85,7 @@ class polynomFunction(potentialFunction):
     dummy=0.0
     if (r <= self._parameters['cutoff']):
       for i in range(self._parameters['order']+1):
-        dummy=dummy+self._parameters['c'+str(i)]*r**i
+        dummy=dummy+self._parameters['c'+str(i)]*(self._parameters['cutoff']-r)**i
     return dummy
            
 
@@ -98,7 +98,7 @@ class polynomFunction(potentialFunction):
     dummy=0.0
     if (r <= self._parameters['cutoff']):
       for i in range(self._parameters['order']+1):
-        dummy=dummy+i*self._parameters['c'+str(i)]*r**(i-1)
+        dummy=dummy+i*self._parameters['c'+str(i)]*(self._parameters['cutoff']-r)**(i-1)
     return dummy
     
     
