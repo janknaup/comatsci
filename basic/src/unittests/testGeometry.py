d2dedd5f35789499645080c9cad8b5a36b19e618
Jan M. Knaup <janknaup@gmail.com>
2012-06-28 10:19:58 +0200

    *CHANGED testGeometry.py to use print_function *ADDED unit tests for output of file formats for which no read support is present

--- basic/src/unittests/testGeometry.py
+++ basic/src/unittests/testGeometry.py
@@ -3,6 +3,7 @@ Created on Mar 13, 2012
 
 @author: Jan M. Knaup <janknaup@gmail.com>
 '''
+from __future__ import print_function
 import unittest,numpy,os
 from comatsci import Geometry
 
@@ -56,7 +57,6 @@ class geometryTest(unittest.TestCase):
         self.assertTrue(numpy.allclose(g.Geometry, g2.Geometry), "written geometry coordinates differ")
         self.assertEqual([6,6,1,1,1,1], g2.AtomTypes, "written geometry types differ")
         self.assertEqual(["C","C","H","H","H","H"], g2.AtomSubTypes, "written geometry subtypes differ")
-
     
     
     def testGENio(self):
@@ -123,6 +123,107 @@ class geometryTest(unittest.TestCase):
         self.assertTrue(numpy.allclose(g.Geometry, g2.Geometry), "written geometry coordinates differ")
         self.assertEqual([6,6,1,1,1,1], g2.AtomTypes, "written geometry types differ")
         self.assertEqual(["C","C","H","H","H","H"], g2.AtomSubTypes, "written geometry subtypes differ")
+        
+        
+    def testAimsOut(self):
+        g=Geometry.Geometry()
+        g.readfile("../../../test_regr/unittest_data/geotest1.fmg")
+        g.writeAIMS("test.aims")
+        testfile=open("test.aims")
+        testlines=list(testfile)
+        testfile.close()
+        os.unlink("test.aims")
+        self.assertEqual(g.getAimsString(), "".join(testlines).strip("\n"), "written file corrupted")
+        AtomTypes=["C","C","H","H","H","H"]
+        georef=numpy.array([[ 0.          ,0.      ,    0.        ],
+                            [ 0.          ,0.      ,    2.52278443],
+                            [ 1.78220452  ,0.      ,   -1.0289559 ],
+                            [-1.78220452  ,0.      ,    3.55174032],
+                            [ 1.78220452  ,0.      ,    3.55174032],
+                            [-1.78220452  ,0.      ,   -1.0289559 ]],dtype=float)
+        georef*=Geometry.Angstrom
+        for ii in range(g.Atomcount):
+            tokens=testlines[ii+1].split()
+            self.assertEqual(AtomTypes[ii],tokens[4],"Element type differs")
+            self.assertAlmostEqual(georef[ii][0],float(tokens[1]),6,"X coordinate differs")
+            self.assertAlmostEqual(georef[ii][1],float(tokens[2]),6,"Y coordinate differs")
+            self.assertAlmostEqual(georef[ii][2],float(tokens[3]),6,"Z coordinate differs")
+
+
+    def testTmOut(self):
+        g=Geometry.Geometry()
+        g.readfile("../../../test_regr/unittest_data/geotest1.fmg")
+        g.writeTurboMole("test.tm")
+        testfile=open("test.tm")
+        testlines=list(testfile)
+        testfile.close()
+        os.unlink("test.tm")
+        self.assertEqual(g.tmString().strip(), "".join(testlines).strip(), "written file corrupted")
+        AtomTypes=["C","C","H","H","H","H"]
+        georef=numpy.array([[ 0.          ,0.      ,    0.        ],
+                            [ 0.          ,0.      ,    2.52278443],
+                            [ 1.78220452  ,0.      ,   -1.0289559 ],
+                            [-1.78220452  ,0.      ,    3.55174032],
+                            [ 1.78220452  ,0.      ,    3.55174032],
+                            [-1.78220452  ,0.      ,   -1.0289559 ]],dtype=float)
+        for ii in range(g.Atomcount):
+            tokens=testlines[ii+1].split()
+            self.assertEqual(AtomTypes[ii],tokens[3],"Element type differs")
+            self.assertAlmostEqual(georef[ii][0],float(tokens[0]),6,"X coordinate differs")
+            self.assertAlmostEqual(georef[ii][1],float(tokens[1]),6,"Y coordinate differs")
+            self.assertAlmostEqual(georef[ii][2],float(tokens[2]),6,"Z coordinate differs")
+    
+    
+    def testFdfOut(self):
+        g=Geometry.Geometry()
+        g.readfile("../../../test_regr/unittest_data/geotest1.fmg")
+        g.writefdf("test.fdf")
+        testfile=open("test.fdf")
+        testlines=list(testfile)
+        testfile.close()
+        os.unlink("test.fdf")
+        reffile=open("../../../test_regr/unittest_data/geotest1.fdf")
+        reflines=list(reffile)
+        for i in range(len(testlines)):
+            self.assertEqual(reflines[i],testlines[i],"written fdf file differs from reference")
+
+
+    def testPDBOut(self):
+        g=Geometry.Geometry()
+        g.readfile("../../../test_regr/unittest_data/geotest1.fmg")
+        g.writepdb("test.pdb")
+        testfile=open("test.pdb")
+        testlines=list(testfile)
+        testfile.close()
+        os.unlink("test.pdb")
+        reffile=open("../../../test_regr/unittest_data/geotest1.pdb")
+        reflines=list(reffile)
+        for i in range(len(testlines)):
+            self.assertEqual(reflines[i],testlines[i],"written pdb file differs from reference")
+            
+            
+    def testXYZQOut(self):
+        g=Geometry.Geometry()
+        g.readfile("../../../test_regr/unittest_data/geotest1.fmg")
+        g.AtomCharges=[0.,1.,3.,7.8220452,5.5174032,-3.14157324839284729457398775532]
+        g.writexyzq("test.xyzq")
+        testfile=open("test.xyzq")
+        testlines=list(testfile)
+        testfile.close()
+        os.unlink("test.xyzq")
+        self.assertEqual(g.pointchargesstring().strip(), "".join(testlines).strip(), "written file corrupted")
+        georef=numpy.array([[ 0.          ,0.      ,    0.        ],
+                            [ 0.          ,0.      ,    2.52278443],
+                            [ 1.78220452  ,0.      ,   -1.0289559 ],
+                            [-1.78220452  ,0.      ,    3.55174032],
+                            [ 1.78220452  ,0.      ,    3.55174032],
+                            [-1.78220452  ,0.      ,   -1.0289559 ]],dtype=float)
+        for ii in range(g.Atomcount):
+            tokens=testlines[ii].split()
+            self.assertAlmostEqual(georef[ii][0],float(tokens[0]),6,"X coordinate differs")
+            self.assertAlmostEqual(georef[ii][1],float(tokens[1]),6,"Y coordinate differs")
+            self.assertAlmostEqual(georef[ii][2],float(tokens[2]),6,"Z coordinate differs")
+            self.assertAlmostEqual(g.AtomCharges[ii],float(tokens[3]),6,"charge differs")
 
 
 def suite():
@@ -132,4 +233,4 @@ def suite():
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testGeometry']
-    unittest.main(verbosity=2)
+    unittest.main()
