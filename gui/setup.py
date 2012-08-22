e9c3b4bc00cc4ab2bea967b5d586ada375643582
Jan M. Knaup <janknaup@gmail.com>
2012-08-22 17:15:19 +0200

    *COMPLETE REWRITE of geostats - Not fully functional yet! *ADDED geostatspack4 python package for QT4 geostats *ADDED geostatspack4_ui directory containing designer files and resources for Qt4 gui *MODIFIED setup.py to reflect switch to geostatspack4 and rewritten geostats

--- gui/setup.py
+++ gui/setup.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 from distutils.core import setup,Extension
 from distutils.dir_util import remove_tree
-import os
+import os,sys
+
+sys.path.append("../basic/src/comatsci")
+
+from constants import VERSIONPREFIX as COMATSCI_VERSIONPREFIX
+from constants import VERSION as COMATSCI_VERSION
+
+try:
+	from bzrlib.branch import BzrBranch
+	branch = BzrBranch.open_containing('.')[0]
+	if branch.nick!="trunk":
+		VERSIONTAG=COMATSCI_VERSIONPREFIX+"-{1:s}-{0:d}".format(branch.last_revision_info()[0],branch.nick)
+	else:
+		VERSIONTAG=COMATSCI_VERSIONPREFIX+"-{0:d}".format(branch.last_revision_info()[0])
+except:
+		VERSIONTAG=COMATSCI_VERSIONPREFIX
 
 startpath=os.getcwd()
 
-VERSIONTAG="1.1.0-rc2"
+
 AUTHOR="Jan M. Knaup"
-AU_EMAIL="Knaup@bccms.uni-bremen.de"
+AU_EMAIL="janknaup@gmail.com"
 URL="http://www.bccms.uni-bremen.de/en/people/home/j_m_knaup/software/"
 
 # define a list of script aliases, these will be installed either by copying
@@ -17,8 +32,8 @@ URL="http://www.bccms.uni-bremen.de/en/people/home/j_m_knaup/software/"
 
 distrib=setup (	name="comatsci-gui",
 		version=VERSIONTAG,
-		packages=['geostatspack'],
-		package_dir={'geostatspack':'src/geostatspack'},
+		packages=['geostatspack4'],
+		package_dir={'geostatspack4':'src/geostatspack4'},
 		scripts=['src/scripts/geostats',],
 		data_files=[("share/doc/comatsci",["doc/comatsci-gui.pdf"])],
 		description="Computational Materials Science Toolkit GUI utilities",
@@ -35,8 +50,8 @@ distrib=setup (	name="comatsci-gui",
 			'License :: OSI Approved :: Non-Profit Open Software License v3',
 			'Operating System :: POSIX',
 			'Programming Language :: Python',
-			'Programming Language :: Python :: 2.4',
-			'Programming Language :: Python :: 2.5',
+			'Programming Language :: Python :: 2.6',
+			'Programming Language :: Python :: 2.7',
 			'Topic :: Scientific/Enginieering',
 			'Topic :: Scientific/Enginieering :: Physics',
 			'Topic :: Scientific/Enginieering :: Chemistry',
