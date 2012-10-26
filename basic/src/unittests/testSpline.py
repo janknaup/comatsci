6991901c91781771e9fa58562fc3f75520a583bb
Jan M. Knaup <janknaup@gmail.com>
2012-10-26 17:06:24 +0200

    *ADDED unit tests for vector splines *FIXED numpy.allclose calls *ADDED asserts for exception raises to increase coverage. Now at 97%

--- basic/src/unittests/testSpline.py
+++ basic/src/unittests/testSpline.py
@@ -5,7 +5,7 @@ Created on Mar 8, 2012
 '''
 from __future__ import print_function
 import unittest,numpy
-from comatsci import spline
+import comatsci
 
 
 class splineTest(unittest.TestCase):
@@ -18,7 +18,18 @@ class splineTest(unittest.TestCase):
         YY=numpy.sin(XX)                         # nodes
         Y2=numpy.sin(X2)                         # intermediates
         C2=numpy.cos(X2)                         # analytic derivates at intermediates
-        SS=spline.Spline(XX,YY)
+        # test exceptions first
+        SS=comatsci.spline.Spline()
+        self.assertRaises(ValueError, SS.setdata, XX, YY[2:]) # different length arrays
+        self.assertRaises(ValueError, SS.setdata, YY, XX)     # non-monotonic x array
+        self.assertRaises(ValueError, SS.splint, 2.5)         # uninitialzed spline
+        self.assertRaises(ValueError, SS.splder, 2.5)         # uninitialized spline
+        # now properly initialize 
+        SS=comatsci.spline.Spline(XX,YY)
+        # some more exceptions
+        self.assertRaises(ValueError, SS.splint, 11)         # x out of range
+        self.assertRaises(ValueError, SS.splder, 11)         # x out of range
+        # now test interpolation
         II=numpy.array([ SS.splint(x) for x in XX ],dtype=float)
         I2=numpy.array([ SS.splint(x) for x in X2 ],dtype=float)
         ID2=numpy.array([ SS.splder(x) for x in X2 ],dtype=float)
@@ -30,26 +41,83 @@ class splineTest(unittest.TestCase):
     
     def testRenner(self):
         """Test Renner Spline interpolation"""
-        ## Renenwr splines use their own parameter range, so the original parameter must be saved
+        ## Renner splines use their own parameter range, so the original parameter must be saved
         ## with the spline, if one wants to compare to original values
         XX=numpy.arange(0,10,0.2,dtype=float)
         SYY=numpy.sin(XX)
         CYY=numpy.cos(XX)
         VEC=numpy.transpose(numpy.array([XX,SYY,CYY]),)
-        RR=spline.RennerSpline(VEC)
+        # uninitialized first
+        RR=comatsci.spline.RennerSpline()
+        self.assertRaises(ValueError, RR.setNodes, VEC[0:4])  # too few nodes
+        self.assertRaises(ValueError, RR.splint, 5)           # uninitialized
+        self.assertRaises(ValueError, RR.splder, 5)           # uninitialized
+        # proper init now
+        RR=comatsci.spline.RennerSpline(VEC)
+        # more errors
+        self.assertRaises(ValueError, RR.splint, -0.1)        # out of range
+        self.assertRaises(ValueError, RR.splder, -0.1)        # out of range
+        self.assertRaises(ValueError, RR.splint, RR.getTotalLength()+0.1)        # out of range
+        self.assertRaises(ValueError, RR.splder, RR.getTotalLength()+0.1)        # out of range
+        # test interpolation
         II=numpy.array([RR.splint(t) for t in numpy.arange(0,RR.totalLength,0.2)])
+        DD=numpy.array([RR.splder(t) for t in numpy.arange(0,RR.totalLength,0.2)])
         IX=numpy.transpose(II)[0]
         IYS=numpy.transpose(II)[1]
         IYC=numpy.transpose(II)[2]
+        DX=numpy.transpose(DD)[0]
+        DYDS=numpy.transpose(DD)[1]
         SIX=numpy.sin(IX)
         CIX=numpy.cos(IX)
-        self.assertTrue(numpy.allclose(IYS[1:-2], SIX[1:-2],5E-4,2E-3),"sine interpolation deviation too large")
+        self.assertTrue(numpy.allclose(IYS[1:-2], SIX[1:-2],1.0+(5E-4),2E-3),"sine interpolation deviation too large")
         # test cosine coordinate
-        self.assertTrue(numpy.allclose(IYC[1:-2], CIX[1:-2], 5E-4,2E-3),"cosine interpolation deviation too large")
-#        pf=open("renner.dat","w")
-#        for ii in range(len(IX)):
-#            print(IX[ii], IYS[ii], SIX[ii], IYC[ii], CIX[ii], file=pf)
-#        pf.close() 
+        self.assertTrue(numpy.allclose(IYC[1:-2], CIX[1:-2], 1.0+(5E-4),2E-3),"cosine interpolation deviation too large")
+        # test derivative
+        self.assertTrue(numpy.allclose(DYDS[1:-2]/DX[1:-2], CIX[1:-2], 1.0+5E-3,1E-1),"sine derivative deviation too large")
+
+
+    def testVecSpline(self):
+        """Test Vector Spline interpolation"""
+        ## Renenwr splines use their own parameter range, so the original parameter must be saved
+        ## with the spline, if one wants to compare to original values
+        XX=numpy.arange(0,10,0.2,dtype=float)
+        SYY=numpy.sin(XX)
+        CYY=numpy.cos(XX)
+        # uninitialized first
+        VV=comatsci.spline.vectorSpline()
+        self.assertRaises(ValueError, VV.setNodes, [XX, [SYY[2:],]])  # different x,y lengths
+        self.assertRaises(ValueError, VV.setNodes, [XX, [ [CYY[i],SYY[i] ] for i in range(len(CYY)-1) ] + [[CYY,]]])  # different y lengths per dimension
+        self.assertRaises(ValueError, VV.setNodes, [SYY, numpy.transpose(numpy.array([CYY,SYY]))]) # non-monotonic x-values
+        self.assertRaises(ValueError, VV.splint, 5)           # uninitialized
+        self.assertRaises(ValueError, VV.splder, 5)           # uninitialized
+        self.assertRaises(ValueError, VV.getArcLength)        # uninitialized
+        # proper init now
+        VV=comatsci.spline.vectorSpline([XX,numpy.transpose(numpy.array([SYY,CYY]))])
+        # more errors
+        self.assertRaises(ValueError, VV.splint, -0.1)        # out of range
+        self.assertRaises(ValueError, VV.splder, -0.1)        # out of range
+        self.assertRaises(ValueError, VV.splint, 11)        # out of range
+        self.assertRaises(ValueError, VV.splder, 11)        # out of range
+        self.assertRaises(ValueError, VV.getArcLength, points=(-1,5)) # out of range
+        self.assertRaises(ValueError, VV.getArcLength, points=(5,15)) # out of range
+        # test interpolation
+        self.assertAlmostEqual(9.79970388534, VV.getArcLength(tolerance=1e-6), 2, "arc length differs")
+        self.assertAlmostEqual(VV.getArcLength(tolerance=1e-6),VV.getArcLength(tolerance=1e-6), 10, "consecutive arc lenghts differ")
+        self.assertEquals(VV.getDimension(), 2, "interpolated vector dimension differs")
+        self.assertEquals(VV.getNodeCount(), len(XX), "Spline node count differs")
+        IX=XX[3:-3]+0.12
+        II=numpy.array([VV.splint(t) for t in IX ] )
+        DD=numpy.array([VV.splder(t) for t in IX ] )
+        IYS=numpy.transpose(II)[0]
+        IYC=numpy.transpose(II)[1]
+        DYDS=numpy.transpose(DD)[0]
+        SIX=numpy.sin(IX)
+        CIX=numpy.cos(IX)
+        self.assertTrue(numpy.allclose(IYS[1:-2], SIX[1:-2],atol=1E-6),"sine interpolation deviation too large")
+        # test cosine coordinate
+        self.assertTrue(numpy.allclose(IYC[1:-2], CIX[1:-2],atol=5E-6),"cosine interpolation deviation too large")
+        # test derivative
+        self.assertTrue(numpy.allclose(DYDS[6:-6], CIX[6:-6], 1.0+1E-5, 1E-5),"sine derivative deviation too large")
 
 
 def suite():
@@ -59,3 +127,4 @@ def suite():
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testSpline']
     unittest.main()
+    
